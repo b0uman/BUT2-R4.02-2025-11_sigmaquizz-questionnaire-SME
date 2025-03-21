1c2e78bd149aac6ccacb51fac87c96080fb70c65
ktshibola <kurtystshibola@gmail.com>
2025-03-21 14:13:50 +0100

    dev

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr11/jeuQuizz/App.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr11/jeuQuizz/App.java
@@ -4,7 +4,10 @@ package org.univ_paris8.iut.montreuil.qdev.tp2025.gr11.jeuQuizz;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+
+    public static void main(String[] args)
+    {
         System.out.println("Hello World!");
+
     }
 }
